c7c703ca21aabb0c75b3734c32edd8167f4ac378
Apollek <Stefaniak.tomasz.ta@gmail.com>
2022-11-23 19:26:38 +0100

    nietoperz

---
+++ src/main/java/com/example/demo/ElektrodaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElektrodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElektrodaApplication.class, args);
+	}
+
+}
