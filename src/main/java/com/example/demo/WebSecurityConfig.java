8424ccee9514d55f829a0a1d2a0c67f9de560930
Bartlomiej-Kozlowski <104801@stud.uz.zgora.pl>
2023-01-07 17:46:35 +0100

    Commented out Security dependencies

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,4 +40,4 @@ public class WebSecurityConfig {
 
 		return new InMemoryUserDetailsManager(user);
 	}
-}
+}*/
